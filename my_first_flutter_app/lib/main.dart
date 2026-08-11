21ba19a9468889b8869c9dc558d0d9a9030a28ff
EmJayAA <martinjonas.aranzado@dbtc-cebu.edu.ph>
2026-08-11 19:20:51 +0800

    feat: added new widgets and a new card for vertical layout

--- my_first_flutter_app/lib/main.dart
+++ my_first_flutter_app/lib/main.dart
@@ -48,10 +48,12 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         centerTitle: true,
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Card(
+          child: Column(
+            children: [
+            Card(
             elevation: 8,
             color: const Color(0xFF2A0808),
             shape: RoundedRectangleBorder(
@@ -66,6 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   // Circular ID Profile Picture
                   ClipOval(
@@ -81,17 +84,19 @@ class _MyHomePageState extends State<MyHomePage> {
                     'Martin Jonas A. Aranzado',
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      fontSize: 22,
+                      fontSize: 24,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                      fontFamily: 'Montserrat',
+                      color: Colors.yellow,
                     ),
                   ),
                   const SizedBox(height: 12),
                   const Text(
                     'BSIT - 3',
                     style: TextStyle(
-                      fontSize: 18,
+                      fontSize: 20,
                       fontWeight: FontWeight.w500,
+                      fontFamily: 'Helvetica',
                       color: Colors.white70,
                     ),
                   ),
@@ -100,8 +105,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     'My First Flutter Application',
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 18,
                       fontStyle: FontStyle.italic,
+                      fontFamily: 'Helvetica',
                       color: Colors.white,
                     ),
                   ),
@@ -110,15 +116,164 @@ class _MyHomePageState extends State<MyHomePage> {
                     'August 1, 2026',
                     style: TextStyle(
                       fontSize: 15,
+                      fontFamily: 'Helvetica',
                       color: Colors.white60,
                     ),
                   ),
+                  const SizedBox(height: 12),
+                  const Text(
+                    'Nicknames: MJ or Emjay',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Helvetica',
+                      color: Colors.white,
+                    )
+                  ),
+                  const SizedBox(height: 12),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Birthdate: March 29, 2005',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: 'Helvetica',
+                          color: Colors.white60,)
+                          ),
+                      SizedBox (width: 10),
+                       Text('Age: 21',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: 'Helvetica',
+                          color: Colors.white60,)
+                          ),
+                    ],
+                  )
                 ],
               ),
             ),
           ),
+
+          const SizedBox(height: 20),
+
+          Card(
+            elevation: 8,
+            color: const Color(0xFF2A0808),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: const BorderSide(
+                color: Color(0xFFFFD700),
+                width: 1.5,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 25.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'ABOUT ME',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat',
+                      color: Colors.yellow,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Divider(
+                    color: Color(0xFFFFD700), 
+                    thickness: 1,
+                  ),
+                  const SizedBox(height: 10),
+                  const SizedBox(height: 12),
+                  const Text(
+                    'Educational Background:',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Helvetica',
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                      'assets/SRCDC Logo.png',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
+                      SizedBox (width: 20),
+                      Image.asset(
+                      'assets/DBTC Logo.png',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    )
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Future Ambition:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Helvetica',
+                          color: Colors.white,)
+                          ),
+                      SizedBox (width: 10),
+                      Expanded(
+                       child: Text('Future Salesian with useful IT Skills and Knowledge.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Helvetica',
+                          color: Colors.white60,)
+                          ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Favorite Quote:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Helvetica',
+                          color: Colors.white,)
+                          ),
+                      SizedBox (width: 10),
+                      Expanded(
+                       child: Text("\"If it is not God\'s will, we cannot force it, but if it is God\'s will, we cannot stop it.\"",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Helvetica',
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white60,)
+                          ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+            ]
         ),
       ),
+    ),
     );
   }
 }
